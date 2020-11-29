d02e0b5dbb816f0dad17dd0845b6744e44ef7e89
jone <jone@163.com>
2020-11-29 17:45:23 +0800

    update check_mysql.sh

--- 脚本/check_mysql.sh
+++ 脚本/check_mysql.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#ps -C(command)：显示指令(进程)名称  --no-header：等同h,不显示标题 
+#wc(word count)  -l(lines)： -w(words)：统计字数 -c(chars)：统计字节数
 if [ $(ps -C mysqld --no-header | wc -l) -eq 0 ]; then
      systemctl restart mysqld
 fi
